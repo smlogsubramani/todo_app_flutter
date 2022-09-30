b0d5790f395623f96836526d62812a12e5012574
unknown <Administrator@SysB18.cc02tree.com>
2022-09-30 14:38:01 +0530

    App bar commit

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const Color tdRed = Color.fromARGB(0, 224, 31, 31);
+const Color tdRed = Color.fromARGB(0, 0, 0, 0);
 
-const Color tdBlue = Color.fromARGB(0, 224, 31, 31);
+const Color tdBlue = Color.fromARGB(0, 255, 255, 255);
 
-const Color tdPink = Color.fromARGB(0, 224, 31, 31);
+const Color tdPink = Color.fromARGB(0, 214, 15, 181);
 
-const Color tdYellow = Color.fromARGB(0, 224, 31, 31);
+const Color tdYellow = Color.fromARGB(0, 224, 210, 8);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
-
+// import 'package:todoapp/constants/colors.dart';
 class Home extends StatelessWidget{
   
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text('Todo App')),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children:
+         [Icon
+          (Icons.menu,
+          color: Colors.white,
+          size: 30,),
+          Container(
+            height: 50,
+            width: 50,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.asset('assests/images/logo.png'),
+            ),
+          )
+        ]),
+        ),
         body:Container(
-        child: Text('this is home screen'),
+        child: Text('This is a todo app'),
       )
     );
   }
