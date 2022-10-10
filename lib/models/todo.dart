ef6c602b5d4ad009221c0047c412946ae8e4e8cd
unknown <Administrator@SysB18.cc02tree.com>
2022-10-10 14:31:42 +0530

    apk built

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -16,12 +16,7 @@ class ToDo{
 
 static List<ToDo>  todoList(){
   return[
-    ToDo(id: '01', todoText: 'morning excersise', isDone: true),
-    ToDo(id: '02', todoText: 'workout', isDone: true),
-    ToDo(id: '03', todoText: 'breakfast',),
-    ToDo(id: '04', todoText: ' lunch', ),
-    ToDo(id: '05', todoText: ' codeing', ),
-    ToDo(id: '06', todoText: ' dinner', ),
+   
   ];
 }
 }
